af9a56cb2b40ebf3dda6104de127fc7d52b50f18
ahmadfirdaus52 <37030208+ahmadfirdaus52@users.noreply.github.com>
2019-03-22 14:08:23 +0700

    Create soal4v2.c

---
+++ soal4/soal4v2.c
@@ -0,0 +1,66 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
+#include <syslog.h>
+#include <string.h>
+
+#include <time.h>
+//template from modul 2 daemon
+int main() {
+  pid_t pid, sid;
+
+  pid = fork();
+
+  if (pid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (pid > 0) {
+    exit(EXIT_SUCCESS);
+  }
+
+  umask(0);
+
+  sid = setsid();
+
+  if (sid < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  if ((chdir("/")) < 0) {
+    exit(EXIT_FAILURE);
+  }
+
+  close(STDIN_FILENO);
+  close(STDOUT_FILENO);
+  close(STDERR_FILENO);
+
+  int count=1;
+  while(1) 
+  {
+    
+    char dir[]="/home/panda/Documents/makanan/makan_enak.txt";
+    struct stat sb;
+    stat(dir, &sb);
+    
+    if(difftime(time(NULL),sb.st_atime)<=30)
+    {
+      char new[100];
+      sprintf(new, "%d.txt", count);
+
+      char dir2[]="/home/panda/Documents/makanan/makan_sehat";
+      strcat(dir2, new);
+      
+      FILE *tmp = fopen(dir2, "w"); 
+      fclose(tmp);
+      count++;
+    }
+    sleep(5);
+  }
+  
+  exit(EXIT_SUCCESS);
+}
